db5339a46c26bc03255c6f731d9831d50eca6fdf
Geatweard <bladeemorph23@proton.me>
2025-12-05 23:06:29 +0000

    main.py accepts book text file and prints contents

---
+++ main.py
@@ -0,0 +1,12 @@
+#Takes file path of book.txt and returns book as string
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+        return file_contents
+
+#prints book text
+def main():
+    frankenstein = get_book_text("./books/frankenstein.txt")
+    print(frankenstein)
+
+main()
